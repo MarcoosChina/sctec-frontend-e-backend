ebc71ab3830b73ff71dc3dc81618bfde68559582
Marcos Vinicius Baumgartner <94867160+MarcoosChina@users.noreply.github.com>
2026-05-21 16:51:37 -0300

    trocarCor
    criei trocarcor.html e trocarcor.js, com o html criei divs, e com o js, ao pressionar o botão, troca as cores das divs

---
+++ Aula_JS/trocarcor.js
@@ -0,0 +1,9 @@
+function trocarCor(){
+    const div1 = document.getElementById("div1");
+    const div2 = document.getElementById("div2");
+    const div3 = document.getElementById("div3");
+
+    div1.style.backgroundColor = "#a5f181";
+    div2.style.backgroundColor = "#ff9100";
+    div3.style.backgroundColor = "#b56bc4";
+}
