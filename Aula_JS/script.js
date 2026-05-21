38c35c212622d3db174174edd493b4c3f0f01524
Marcos Vinicius Baumgartner <94867160+MarcoosChina@users.noreply.github.com>
2026-05-21 01:04:51 -0300

    js

---
+++ Aula_JS/script.js
@@ -0,0 +1,11 @@
+function varTeste1()
+{
+    var num1 = 10;
+    if (num1 >= 10){
+        console.log("O número é maior ou igual a 10");
+    }
+    else{
+        console.log("O número é menor que 10");
+    }
+}
+varTeste1();
