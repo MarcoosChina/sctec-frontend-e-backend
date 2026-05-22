68461950532f24404408562fc8c78d63de906a03
Marcos Vinicius Baumgartner <94867160+MarcoosChina@users.noreply.github.com>
2026-05-22 11:02:43 -0300

    corrigindo

---
+++ Aula_JS/array.js
@@ -0,0 +1,7 @@
+const listadecompras = ["leite", "pão", "queijo", "presunto", "manteiga"];
+console.log(listadecompras);
+console.log(listadecompras[0]);
+console.log(listadecompras[1]);
+console.log(listadecompras[2]);
+console.log(listadecompras[3]);
+console.log(listadecompras[4]);
