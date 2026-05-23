1f4e5ac3693394c35c9af443d8de08881486daca
Marcos Vinicius Baumgartner <94867160+MarcoosChina@users.noreply.github.com>
2026-05-23 00:09:38 -0300

    condições

---
+++ Aula_JS/condicao.js
@@ -0,0 +1,29 @@
+const carros = ["Ferrari", "Lamborghini", "Porsche", "Bugatti", "McLaren"];
+
+const conteudo = document.getElementById("conteudo");
+
+let dados = "";
+let i = 0;
+
+while (i < carros.length){
+    dados += "<p>" + carros[i] + "</p>";
+    i ++;
+}
+
+const secao = document.createElement("div");
+secao.innerHTML = "<h2>Loop while</h2>" + dados;
+conteudo.appendChild(secao);
+// O código acima cria uma lista de carros de luxo e exibe essa lista
+// em um elemento HTML com o id "conteudo". Ele utiliza um loop while 
+// para iterar sobre os elementos do array "carros" e construir uma string
+//  HTML que é então inserida em um elemento div.
+
+let carro1 = {marca: "Corsa", modelo: "Sedan", ano: 2002};
+let carro2 = {marca: "Gol", modelo: "Hatch", ano: 2010};
+let carro3 = {marca: "Palio", modelo: "Hatch", ano: 2005};
+
+let carros2 = [];
+carros2.push(carro1);
+carros2.push(carro2);
+carros2.push(carro3);
+
