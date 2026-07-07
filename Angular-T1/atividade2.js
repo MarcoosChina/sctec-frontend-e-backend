7c96b3b83feb8f6515b296b7520c1e83ba3b126b
Marcos Vinicius Baumgartner <94867160+MarcoosChina@users.noreply.github.com>
2026-07-06 21:40:49 -0300

    atividade2 js

---
+++ Angular-T1/atividade2.js
@@ -0,0 +1,7 @@
+//criar uma variavel chamada com o valor "eu amo programar em Java" 
+//e substitua a palavra Java por JavaScript e exiba no console
+
+var valor = "eu amo programar em Java";
+console.log(valor);
+valor = valor.replace("Java", "JavaScript");
+console.log(valor);
