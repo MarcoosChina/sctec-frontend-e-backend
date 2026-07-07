31c13fa6fd7f258550f4537fb963850686fb40c3
Marcos Vinicius Baumgartner <94867160+MarcoosChina@users.noreply.github.com>
2026-07-06 21:07:33 -0300

    atividade1 JS

---
+++ Angular-T1/atividade1.js
@@ -0,0 +1,9 @@
+//receba o nome de uma cidade e coloque em maiusculo e minusculo
+
+var cidade = "Blumenau";
+
+cidade = cidade.toUpperCase();
+console.log(cidade)
+cidade = cidade.toLowerCase();
+console.log(cidade)
+
