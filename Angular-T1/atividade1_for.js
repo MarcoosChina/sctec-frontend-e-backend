4ab848e882257e66ed60f47036f728edbc0265bd
Marcos Vinicius Baumgartner <94867160+MarcoosChina@users.noreply.github.com>
2026-07-09 20:59:06 -0300

    atividade for 2

--- Angular-T1/atividade1_for.js
+++ Angular-T1/atividade1_for.js
@@ -1,5 +1,7 @@
 //somar os números pares de 1 a 100
 
+// o += faz a mesma coisa que soma = soma + i
+// o i += 2 faz a mesma coisa que i = i + 2
 let soma = 0;
 
 for (let i = 2; i < 101; i += 2){
---
+++ Angular-T1/atividade2_for.js
@@ -0,0 +1,11 @@
+//receba um numero digitado pelo usuario
+//calcular fatorial
+
+let num = 4;
+let resultado = 1;
+
+for (let i = 1; i <= num; i++ ){
+    resultado *= i;
+    console.log(resultado)
+}
+
