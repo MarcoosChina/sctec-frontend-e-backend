56bebd1aa088679f61e3a3f0ab20467bb2abfddc
Marcos Vinicius Baumgartner <94867160+MarcoosChina@users.noreply.github.com>
2026-07-09 21:53:53 -0300

    Add number guessing game implementation

---
+++ Angular-T1/atividade1_while.js
@@ -0,0 +1,20 @@
+//gerar numero aleatório de 1 a 100 
+//pedir ao usuario para tentar adivinhar o numero via prompt
+//caso acertar, finalizar o programa
+//caso errar, informar se o numero é maior ou menor que o numero digitado
+
+const prompt = require('prompt-sync')();
+
+let numAleatorio = Math.round(Math.random() * 100 + 1);
+let numeroUsuario = parseInt(prompt("Tente adivinhar o número entre 1 e 100:"));
+
+while(numeroUsuario != numAleatorio){
+    if(numeroUsuario < numAleatorio){
+        console.log("O número é maior que " + numeroUsuario);
+    }else{
+        console.log("O número é menor que " + numeroUsuario);
+    }
+    numeroUsuario = parseInt(prompt("Tente adivinhar o número entre 1 e 100:"));
+}
+
+console.log("Parabéns! Você acertou o número " + numAleatorio + "!");
