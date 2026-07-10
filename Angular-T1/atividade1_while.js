a61d1fdf82f1dc4ac32abbb4e45479b8a1408285
Marcos Vinicius Baumgartner <94867160+MarcoosChina@users.noreply.github.com>
2026-07-09 21:57:47 -0300

    Refactor number guessing logic for clarity and remove redundant code

--- Angular-T1/atividade1_while.js
+++ Angular-T1/atividade1_while.js
@@ -2,19 +2,14 @@
 //pedir ao usuario para tentar adivinhar o numero via prompt
 //caso acertar, finalizar o programa
 //caso errar, informar se o numero é maior ou menor que o numero digitado
-
 const prompt = require('prompt-sync')();
 
 let numAleatorio = Math.round(Math.random() * 100 + 1);
 let numeroUsuario = parseInt(prompt("Tente adivinhar o número entre 1 e 100:"));
 
 while(numeroUsuario != numAleatorio){
-    if(numeroUsuario < numAleatorio){
-        console.log("O número é maior que " + numeroUsuario);
-    }else{
-        console.log("O número é menor que " + numeroUsuario);
-    }
+    numeroUsuario  > numAleatorio ? console.log("O número é menor que " + numeroUsuario) : console.log("O número é maior que " + numeroUsuario);
     numeroUsuario = parseInt(prompt("Tente adivinhar o número entre 1 e 100:"));
 }
 
-console.log("Parabéns! Você acertou o número " + numAleatorio + "!");
+console.log("Parabéns! Você acertou o número " + numAleatorio);
