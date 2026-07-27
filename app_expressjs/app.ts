e745c2c96b709a89a6c00d12b1d14c96773b9202
Marcos Vinicius Baumgartner <94867160+MarcoosChina@users.noreply.github.com>
2026-07-26 21:45:47 -0300

    estudando afundo sobre express js

--- app_expressjs/app.ts
+++ app_expressjs/app.ts
@@ -1,6 +1,16 @@
+//entrar na pasta app_expresjs e rodar o comando: npm install
+
 import express from "express";
+import path from "path";
 
 const app = express();
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 })
