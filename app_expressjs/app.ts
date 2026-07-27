131c731a21fee0968ee1a94c4ffa5c9715689ab3
Marcos Vinicius Baumgartner <94867160+MarcoosChina@users.noreply.github.com>
2026-07-26 23:51:01 -0300

    estudando rotas

--- app_expressjs/app.ts
+++ app_expressjs/app.ts
@@ -11,6 +11,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/about", (req, res) =>{
+  res.sendFile(path.join(__dirname, "public", "about.html"));
+})
+
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
 });
